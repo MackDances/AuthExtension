ad958f245df7df70f5b5d2567c5e973a9f4b9f7e
Vachan MN <vachanmn123@gmail.com>
2022-10-20 11:07:20 +0530

    comments, tiny changes

--- addSecret.js
+++ addSecret.js
@@ -1,21 +1,32 @@
+// Get the secret and set the status element.
 chrome.storage.sync.get(['secret'], function(result) {
         if (result.secret) {
+            // If the secret exists, set the status to "Secret is loaded".
             document.getElementById('status').innerHTML = "Status: Secret is loaded!";
             document.getElementById('status').style.color = "green";
         } else {
+            // If the secret does not exist, set the status to "Secret is not loaded".
             document.getElementById('status').innerHTML = "Status: Secret is not loaded!";
             document.getElementById('status').style.color = "red";
         }
+        // Add the input fields for setting a new secret.
         document.getElementById('addSecret').innerHTML = `<input id="newSecret"><button id="setSecret">Set Secret</button>`;
+        // When the setSecret button is clicked, call the setSecretToStorage function.
         document.getElementById('setSecret').addEventListener("click", setSecretToStorage)
 });
 
+// This function sets the secret entered to the storage.
 function setSecretToStorage() {
+    // Get the secret that was entered.
     let inpSecret = document.getElementById('newSecret').value
+    // Set the secret to storage.
     chrome.storage.sync.set({secret: inpSecret}, function() {
+        // After the secret is stored, set the status to "Secret is loaded".
         document.getElementById('status').innerHTML = "Status: Secret is loaded!";
         document.getElementById('status').style.color = "green";
+        // Generate a new OTP.
         let otp = getToken(inpSecret, {period: 30});
+        // remove the input and button and show new OTP instead.
         document.getElementById('addSecret').innerHTML = `<p>OTP is: ${otp}</p>`
     })
 }
--- home.js
+++ home.js
@@ -1,18 +1,36 @@
+// When the extension is opened, automatically generate an OTP.
 putOutput()
+// If the generate button is pressed, generate an OTP.
 document.getElementById('generateBtn').addEventListener("click", putOutput);
+// If the copy button is pressed, copy the OTP.
 document.getElementById('copyToClipboard').addEventListener("click", copy);
 
+// This function sets the output element to display the OTP.
 function putOutput() {
+    // Get the secret from the storage.
     chrome.storage.sync.get(['secret'], function(result) {
-        console.log(result.secret);
+        if (!result.secret) {
+            // If the secret isn't present, put an error message as the output and disable the buttons and exit.
+            document.getElementById('output').innerHTML = "The Secret has not been set.";
+            document.getElementById('output').style.color = "red";
+            document.getElementById('generateBtn').disabled = true;
+            document.getElementById('copyToClipboard').disabled = true;
+            return;
+        }
+        // Generate the OTP using the secret and the time seed of 30 seconds(standard).
         let secret = result.secret;
         let out = getToken(secret, {period: 30});
+        // Set the output element to show the OTP.
         document.getElementById('output').innerHTML = out;
     });
 }
 
+// This function copies the OTP from the output element.
 function copy() {
+    // Get the OTP from the output element.
     let otp = document.getElementById('output').innerHTML;
+    // Copy the OTP to the clipboard.
     navigator.clipboard.writeText(otp);
+    // Change the button's text to say copied.
     document.getElementById('copyToClipboard').innerHTML = "copied!";
 }
